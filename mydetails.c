d3f2dd97f7df2b46cd78db42c9765c6fcaafdfed
Harshithakt9122006 <lleelavathiha@gmail.com>
2025-05-13 21:27:04 +0530

    mydetails.c

---
+++ mydetails.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int age,usn,weight;
+    char name,college_name;
+    printf("enter your name\n");
+    scanf("%s",&name);
+    printf("enter your college_name\n");
+    scanf("%s",&college_name);
+    printf("enter your age\n");
+    scanf("%d",&age);
+    printf("enter your usn\n");
+    scanf("%d",&usn);
+    printf("enter your weight\n");
+    scanf("%d",&weight);
+    return 0;
+}

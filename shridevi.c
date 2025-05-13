83e8aa5adc20a74ed9b9d012e2dc4d278f58db2a
Harshithakt9122006 <lleelavathiha@gmail.com>
2025-05-13 15:41:30 +0530

    shridevi.c

---
+++ shridevi.c
@@ -0,0 +1,14 @@
+// function declaration and calling
+#include<stdio.h>
+int shridevi()
+{
+    printf("Harshitha\n");
+    printf("age\n");
+    printf("date of birth\n");
+    printf("mobile number");
+}
+int main()
+{
+    shridevi();
+    return 0;
+}

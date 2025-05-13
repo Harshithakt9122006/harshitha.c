b2f759917eef8a51fbd7904c15d6a3604660f592
Harshithakt9122006 <lleelavathiha@gmail.com>
2025-05-13 20:44:56 +0530

    logicalnot.c

---
+++ logicalnot.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("enter a number");
+    scanf("%d",&num);
+    if (!(num>0))
+    {
+        printf("the number is not positive\n");
+    }
+    else
+    {
+        printf("the number is positive");
+    }
+return 0;
+}

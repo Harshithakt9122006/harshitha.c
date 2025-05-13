38d3a8d0f3e5ae4ff243fe2b669404dae648ba1e
Harshithakt9122006 <lleelavathiha@gmail.com>
2025-05-13 21:10:31 +0530

    int_and_float.c

---
+++ declare_int_and_float.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int intvalue;
+    float floatvalue;
+    printf("enter an integer\n");
+    scanf("%d",&intvalue);
+    printf("enter a float value\n");
+    scanf("%f",&floatvalue);
+    printf("you enter integer %d\n",intvalue);
+    printf("you entered float %f\n",floatvalue);
+    return 0;
+}

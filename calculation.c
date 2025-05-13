5eba46215589d991b9cbff96279d29c6345b8631
Harshithakt9122006 <lleelavathiha@gmail.com>
2025-05-13 16:03:58 +0530

    calculation.c

---
+++ calculation.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int sum,difference,product,quotient;
+int a,b;
+printf("Enter the values of a and b\n");
+scanf("%d%d",&a,&b);
+sum=a+b;
+difference=a-b;
+product=a*b;
+quotient=a/b;
+printf("sum=%d\n",sum);
+printf("difference=%d\n",difference);
+printf("product=%d\n",product);
+printf("quotient=%d\n",quotient);
+}

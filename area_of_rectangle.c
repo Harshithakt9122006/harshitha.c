8b888eb4b57d610c7675733b0b011e7fd48af274
Harshithakt9122006 <lleelavathiha@gmail.com>
2025-05-13 19:04:55 +0530

    area_of_rectangle.c

---
+++ area_of_rectangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int area,l,b;
+    printf("Enter the length and beadth");
+    scanf("%d%d",&l,&b);
+    area=l*b;
+    printf("The area of rectangle is %d \n",area);
+    return 0;
+}

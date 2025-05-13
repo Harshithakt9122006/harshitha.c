0150c0b2fae91d64d9fbd2f8030e1e832bb69c36
Harshithakt9122006 <lleelavathiha@gmail.com>
2025-05-13 21:03:36 +0530

    details.c

---
+++ details.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char name[]="ritu";
+    int age=24;
+    char location[]="banglore";
+    printf("name:%s\n",name);
+    printf("age:%d\n",age);
+    printf("location:%s\n",location);
+    return 0;
+}

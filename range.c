549212314c60c6cf600e02ff81c4e8d8528f9a51
Harshithakt9122006 <lleelavathiha@gmail.com>
2025-05-14 14:08:28 +0530

    range.c

---
+++ range.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number");
+    scanf("%d",&num);
+    if(num>=10&&num<=100)
+    {
+        printf("the number is within the range 10 to 100");
+    }
+    else
+    {
+        printf("the number is within the range 10 to 100");
+    }
+    return 0;
+}

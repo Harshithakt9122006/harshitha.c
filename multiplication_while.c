c7afd8006d2ac6ff68360b4d3a1768ede5417527
Harshithakt9122006 <lleelavathiha@gmail.com>
2025-05-14 11:16:53 +0530

    multiplication_while.c

---
+++ multiplication_while.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num,i=1;
+    printf("enter a number ");
+    scanf("%d",&num);
+    while(i<=10)
+    {
+        printf("%d*%d=%d\n",num,i,num*i);
+        i++;
+    }
+}
